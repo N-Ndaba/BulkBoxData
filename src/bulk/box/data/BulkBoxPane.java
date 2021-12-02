5e13ec61dc0521d03b636a0a992a241f6ef8af47
N-Ndaba <Ndumison727@gmail.com>
2021-12-02 08:52:39 +0100

    Create new branch

--- src/bulk/box/data/BulkBoxPane.java
+++ src/bulk/box/data/BulkBoxPane.java
@@ -1,5 +1,7 @@
 package bulk.box.data;
 
+import java.util.ArrayList;
+
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
@@ -7,6 +9,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 public class BulkBoxPane extends StackPane
 {
@@ -17,7 +20,7 @@ public class BulkBoxPane extends StackPane
 		 pane.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
 		 pane.setHgap(7);
 		 pane.setVgap(5.5);
-		 
+		 /*
 		 // Place nodes in the pane
 		 pane.add(new Label("Product:"), 0, 0);
 		 pane.add(new Label("Quantity:"), 1, 0);
@@ -39,7 +42,6 @@ public class BulkBoxPane extends StackPane
 		 pane.add(totalWeight , 4, 1);
 		 
 		 pane.add(new Label("Flare 6 Pack:"), 0, 2);
-		 pane.add(new TextField(), 1, 2);
 		 TextField flareSix = new TextField(); 
 		 pane.add(flareSix, 1, 2);
 		 TextField flareSixBox= new TextField(); 
@@ -56,7 +58,6 @@ public class BulkBoxPane extends StackPane
 		 pane.add(totalSixWeight , 4, 2);
 		 
 		 pane.add(new Label("Sir Beacon 60:"), 0, 3);
-		 pane.add(new TextField(), 1, 3);
 		 TextField BanBeacon  = new TextField(); 
 		 pane.add(BanBeacon, 1, 3);
 		 TextField BanBeaconBox= new TextField(); 
@@ -83,41 +84,132 @@ public class BulkBoxPane extends StackPane
 		 totalSirBeaconportableWeight.setEditable(false);
 		 pane.add(totalSirBeaconportableWeight , 4, 4);
 		 
+		 pane.add(new Label("Mono S:"), 0, 5);
+		 TextField monoQuantity  = new TextField(); 
+		 pane.add(monoQuantity, 1, 5);
+		 TextField monoBoxType = new TextField(); 
+		 monoBoxType.setEditable(false); 
+		 monoBoxType.setAlignment(Pos.CENTER);
+		 pane.add(monoBoxType, 2, 5);
+		 TextField monoDimensions  = new TextField(); 
+		 monoDimensions.setEditable(false);
+		 pane.add(monoDimensions, 3, 5);
+		 TextField monoTotalWeight  = new TextField();
+		 monoTotalWeight.setEditable(false);
+		 pane.add(monoTotalWeight , 4, 5);
 		 
-		 setOnKey(flare, flareBox, FlareDimensions, totalWeight, "Flare", 332);
-		 setOnKey(flareSix, flareSixBox, FlareSixDimensions, totalSixWeight, "Flare 6 pack", 1.9);
-		 setOnKey(BanBeacon, BanBeaconBox, BanBeaconDimensions, totalBanBeaconWeight, "Ban Beacon", 740);
-		 setOnKey(SirBeaconportable, SirBeaconportableBox, SirBeaconportableDimensions, totalSirBeaconportableWeight, "Sir Beacon / portable", 1.1);
+		 pane.add(new Label("Duplo S:"), 0, 6);
+		 TextField duploQuantity  = new TextField(); 
+		 pane.add(duploQuantity, 1, 6);
+		 TextField duploBoxType = new TextField(); 
+		 duploBoxType.setEditable(false); 
+		 duploBoxType.setAlignment(Pos.CENTER);
+		 pane.add(duploBoxType, 2, 6);
+		 TextField duploDimensions  = new TextField(); 
+		 duploDimensions.setEditable(false);
+		 pane.add(duploDimensions, 3, 6);
+		 TextField duploTotalWeight  = new TextField();
+		 duploTotalWeight.setEditable(false);
+		 pane.add(duploTotalWeight , 4, 6);
+		 
+		 pane.add(new Label("Mono/ Duplo Combo:"), 0, 7);
+		 TextField monoduploQuantity  = new TextField(); 
+		 pane.add(monoduploQuantity, 1, 7);
+		 TextField monoduploBoxType = new TextField(); 
+		 monoduploBoxType.setEditable(false); 
+		 monoduploBoxType.setAlignment(Pos.CENTER);
+		 pane.add(monoduploBoxType, 2, 7);
+		 TextField monoduploDimensions  = new TextField(); 
+		 monoduploDimensions.setEditable(false);
+		 pane.add(monoduploDimensions, 3, 7);
+		 TextField monoduploTotalWeight  = new TextField();
+		 monoduploTotalWeight.setEditable(false);
+		 pane.add(monoduploTotalWeight , 4, 7);
+		 
+		 pane.add(new Label("1500S:"), 0, 8);
+		 TextField fsQuantity  = new TextField(); 
+		 pane.add(fsQuantity, 1, 8);
+		 TextField fsBoxType = new TextField(); 
+		 fsBoxType.setEditable(false); 
+		 fsBoxType.setAlignment(Pos.CENTER);
+		 pane.add(fsBoxType, 2, 8);
+		 TextField fsDimensions  = new TextField(); 
+		 fsDimensions.setEditable(false);
+		 pane.add(fsDimensions, 3, 8);
+		 TextField fsTotalWeight  = new TextField();
+		 fsTotalWeight.setEditable(false);
+		 pane.add(fsTotalWeight , 4, 8);
+		 
+		 pane.add(new Label("1500S Vert:"), 0, 9);
+		 TextField fvQuantity  = new TextField(); 
+		 pane.add(fvQuantity, 1, 9);
+		 TextField fvBoxType = new TextField(); 
+		 fvBoxType.setEditable(false); 
+		 fvBoxType.setAlignment(Pos.CENTER);
+		 pane.add(fvBoxType, 2, 9);
+		 TextField fvDimensions  = new TextField(); 
+		 fvDimensions.setEditable(false);
+		 pane.add(fvDimensions, 3, 9);
+		 TextField fvTotalWeight  = new TextField();
+		 fvTotalWeight.setEditable(false);
+		 pane.add(fvTotalWeight , 4, 9);
+		 
+		 pane.add(new Label("2D:"), 0, 10);
+		 TextField tdQuantity  = new TextField(); 
+		 pane.add(tdQuantity, 1, 10);
+		 TextField tdBoxType = new TextField(); 
+		 tdBoxType.setEditable(false); 
+		 tdBoxType.setAlignment(Pos.CENTER);
+		 pane.add(tdBoxType, 2, 10);
+		 TextField tdDimensions  = new TextField(); 
+		 tdDimensions.setEditable(false);
+		 pane.add(tdDimensions, 3, 10);
+		 TextField tdTotalWeight  = new TextField();
+		 tdTotalWeight.setEditable(false);
+		 pane.add(tdTotalWeight , 4, 10);*/
+		
+		// setOnKey(flare, flareBox, FlareDimensions, totalWeight, "Flare", 332);
+		 //setOnKey(flareSix, flareSixBox, FlareSixDimensions, totalSixWeight, "Flare 6 pack", 1.9);
+		 //setOnKey(BanBeacon, BanBeaconBox, BanBeaconDimensions, totalBanBeaconWeight, "Ban Beacon", 740);
+		// setOnKey(SirBeaconportable, SirBeaconportableBox, SirBeaconportableDimensions, totalSirBeaconportableWeight, "Sir Beacon / portable", 1.1);
 			
-		 getChildren().add(pane);
+		 //getChildren().add(pane);
+		
+		 pane.add(new Label("Product:"), 0, 0);
+		 pane.add(new Label("Quantity:"), 1, 0);
+		 pane.add(new Label("BoxType:"), 2, 0);
+		 pane.add(new Label("Dimensions (L x W x H):"), 3, 0);
+		 pane.add(new Label("Total Weight (g | kg):"), 4, 0);
+		 
+		VBox vbEvents = new VBox();
+		vbEvents.getChildren().add(setupGUI("Flare", new TextField(),  new TextField(), new TextField(), new TextField())); 
+		vbEvents.getChildren().add(setupGUI("ntttttttttt", new TextField(),  new TextField(), new TextField(), new TextField())); 
+		getChildren().addAll(pane, vbEvents);
 	}
 	
-	private GridPane createLabel(String productName)
+	public GridPane setupGUI(String label, TextField quantity, TextField boxType, TextField dimensions, TextField totalWeight)
 	{
-		GridPane grid = new GridPane();
-		grid.setVgap(4);
-		grid.setPadding(new Insets(5,5,5,5));
-		
-		
-		grid.add(new Label(productName), 0, 4);
-		//grid.add(new Sir, 1, 4);
-		return grid; 
-		/*
-		 pane.add(new Label("Sir Beacon / portable:"), 0, 4);
-		 //pane.add(new TextField(), 1, 4);
-		 TextField SirBeaconportable  = new TextField(); 
-		 pane.add(SirBeaconportable, 1, 4);
-		 TextField SirBeaconportableBox= new TextField(); 
-		 SirBeaconportableBox.setEditable(false); 
-		 pane.add(SirBeaconportableBox, 2, 4);
-		 TextField SirBeaconportableDimensions  = new TextField(); 
-		 SirBeaconportableDimensions.setEditable(false);
-		 pane.add(SirBeaconportableDimensions, 3, 4);
-		 TextField totalSirBeaconportableWeight  = new TextField();
-		 totalSirBeaconportableWeight.setEditable(false);
-		 pane.add(totalSirBeaconportableWeight , 4, 4);
-		 	
-		 */
+		 GridPane pane = new GridPane();
+		 pane.setAlignment(Pos.CENTER);
+		 pane.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
+		 pane.setHgap(7);
+		 pane.setVgap(5.5);
+		 
+		 
+		 
+		 
+		 pane.add(new Label(label), 0, 1);
+		 pane.add(quantity, 1, 1);
+		 boxType.setEditable(false); 
+		 boxType.setAlignment(Pos.CENTER);
+		 pane.add(boxType, 2, 1);
+		 dimensions.setEditable(false);
+		 pane.add(dimensions, 3, 1);
+		 totalWeight.setEditable(false);
+		 pane.add(totalWeight , 4, 1);
+		 
+		 return pane; 
+		// getChildren().add(pane);
 	}
 	
 	private void setOnKey(TextField txtQuantity, TextField txtBoxType, TextField txtDimensions, TextField txtTotalWeight,  String strProductName, double dblWeight)
